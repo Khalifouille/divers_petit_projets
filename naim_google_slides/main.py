ac85e09869ebf9a70cd9b220f39543442934fa1b
Khalifouille <59296081+Khalifouille@users.noreply.github.com>
2025-03-15 21:07:51 +0100

    test2

---
+++ naim_google_slides/main.py
@@ -0,0 +1,25 @@
+import os
+import google.auth
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+
+SCOPES = ['https://www.googleapis.com/auth/drive.activity.readonly']
+
+def main():
+    creds = None
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                r"F:\divers_petit_projets\naim_google_slides\client_secret_916689696131-obaj2l7v3a9sgjrroo6r5f8fkgn8526m.apps.googleusercontent.com (1).json", SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+if __name__ == '__main__':
+    main()
